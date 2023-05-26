5487339d95564f6f3e4f5cdad3040ac5cb1b2a3e
Vanessa De Oliveira Mello <vanessadeoliveiramello@MacBook-Air-de-Vanessa.local>
2023-05-26 00:46:45 -0300

    Reorganizando a estrutura do algoritmo

--- Tree_BinaryTree/Main.java
+++ Tree_BinaryTree/Main.java
@@ -5,5 +5,21 @@ package Tree_BinaryTree;
  * @author vanessadeoliveiramello
  */
 public class Main {
-    
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+  
+        /* Cria a seguinte árvore binária:
+               1
+             /   \
+            2     3
+           / \
+          4   5
+        */
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+        
+    } 
 }
